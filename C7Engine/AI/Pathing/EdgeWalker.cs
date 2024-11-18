8a30b2ea2cd1dc57538e0a72f0ab0e0af5ce148f
Tom <tomwer3@gmail.com>
2024-11-17 20:25:24 -0600

    EdgeWalker: fix the call to getMovementCost to properly handle reads/rivers
    Using `neighbor` as both arguments means that river crossings and road usefulness was not always properly handled.
    
    One of the added tests fails without this change, thinking that a move from <unroaded tile> to <roaded tile> only costs 1/3 of a movement point:
    
    ```
      Failed EngineTests.WalkerOnLandTest.testRoadOnDestinationNotOnStart [42 ms]
      Error Message:
       Assert.Contains() Failure
    Not found: (filter expression)
    In value:  List<Edge<Tile>> [Edge`1 { current = [0, 0] ( on ), distanceToCurrent = 0.33333334, prev = [0, 0] ( on ) }]
    ```

--- C7Engine/AI/Pathing/EdgeWalker.cs
+++ C7Engine/AI/Pathing/EdgeWalker.cs
@@ -2,19 +2,18 @@ using System.Collections.Generic;
 using C7GameData;
 
 namespace C7Engine.Pathing {
-	public abstract class EdgeWalker<TNode>
-	{
+	public abstract class EdgeWalker<TNode> {
 		public abstract IEnumerable<Edge<TNode>> getEdges(TNode node);
 	}
 
-	public class WalkerOnLand: EdgeWalker<Tile> {
+	public class WalkerOnLand : EdgeWalker<Tile> {
 		public override IEnumerable<Edge<Tile>> getEdges(Tile node) {
 			List<Edge<Tile>> result = new List<Edge<Tile>>();
 			foreach (KeyValuePair<TileDirection, Tile> pair in node.neighbors) {
 				TileDirection direction = pair.Key;
 				Tile neighbor = pair.Value;
 				if (neighbor.IsLand()) {
-					float movementCost = MapUnitExtensions.getMovementCost(neighbor, direction, neighbor);
+					float movementCost = MapUnitExtensions.getMovementCost(node, direction, neighbor);
 					result.Add(new Edge<Tile>(node, neighbor, movementCost));
 				}
 			}
@@ -22,7 +21,7 @@ namespace C7Engine.Pathing {
 		}
 	}
 
-	public class WalkerOnWater: EdgeWalker<Tile> {
+	public class WalkerOnWater : EdgeWalker<Tile> {
 		public override IEnumerable<Edge<Tile>> getEdges(Tile node) {
 			List<Edge<Tile>> result = new List<Edge<Tile>>();
 			foreach (KeyValuePair<TileDirection, Tile> pair in node.neighbors) {
